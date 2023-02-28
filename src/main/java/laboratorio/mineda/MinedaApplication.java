77458b4cf5a4a5268c495d360450c788e30f9e90
Victor Fernandes <89042715+victornaca@users.noreply.github.com>
2023-02-28 19:23:23 -0300

    att

---
+++ src/main/java/laboratorio/mineda/MinedaApplication.java
@@ -0,0 +1,13 @@
+package laboratorio.mineda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/laboratorio/mineda/MinedaApplicationTests.java
@@ -0,0 +1,13 @@
+package laboratorio.mineda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
